6823bfa458b996ff8f9758d4c3cea086ff3ef041
Peter Alexander <peter.d.alexander23@gmail.com>
2022-02-15 22:38:14 +0000

    Add FreeRTOS as submodule, reo  main as RTOS tasks
    Add a pull from FreeRTOS with a CMake config and basic demo app borrowed
    from https://github.com/lowlevellearning/rp2040-freertos

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,74 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
 #include "extra.hpp"
 
-int main()
+#define ONBOARD_LED_PIN 25
+
+//******************************************//
+//              FreeRTOS Tasks              //
+//******************************************//
+
+void heartbeatTask(void *param)
 {
-    uint32_t a = 1;
-    uint32_t b = 2;
+    gpio_init(ONBOARD_LED_PIN);
+    gpio_set_dir(ONBOARD_LED_PIN, GPIO_OUT);
 
-    uint32_t c = a + b;
+    while (1)
+    {
+        gpio_put(ONBOARD_LED_PIN, 1);
+        vTaskDelay(1000);
+        gpio_put(ONBOARD_LED_PIN, 0);
+        vTaskDelay(1000);
+    }
+}
 
+void serialTask(void *param)
+{
     stdio_init_all();
 
     while (1)
     {
         printf("Hello World!\n");
         extra_print();
-        sleep_ms(200);
+        vTaskDelay(200);
     }
+}
 
-    return 0;
+int main()
+{
+    TaskHandle_t heartbeatTaskHdl = NULL;
+    TaskHandle_t serialTaskHdl = NULL;
+
+    uint32_t status;
+
+    status = xTaskCreate(
+        heartbeatTask,
+        "Heartbeat",
+        1024,
+        NULL,
+        tskIDLE_PRIORITY,
+        &heartbeatTaskHdl);
+
+    status = xTaskCreate(
+        serialTask,
+        "Serial",
+        1024,
+        NULL,
+        tskIDLE_PRIORITY + 1,
+        &serialTaskHdl);
+
+    vTaskStartScheduler();
+
+    // SHOULD NOT REACH HERE
+    for (;;)
+    {
+        sleep_ms(1000);
+    }
 }
