941d515ab0364e50ff4338b7bcf3697747134096
Peter Alexander <peter.d.alexander23@gmail.com>
2022-03-04 21:51:27 +0000

    Add a working unit test for Unity itself
    Add a complete Unit Test system that allows an unlimitted number of test
    suites to be compiled and run with CTest.
    
    Add an example suite that demonstrates that Unity is working correctly.

--- test/unity_framework_tests/test_unity_framework.c
+++ test/unity_framework_tests/test_unity_framework.c
@@ -1,8 +1,8 @@
 /**
- * @file    src_name.cpp
- * @author  Author Name (author@email.com)
- * @brief   Source brief
- * @copyright Copyright (c) YEAR
+ * @file    test_unity_config.cpp
+ * @author  Peter Alexander (peter.d.alexander23@gmail.com)
+ * @brief   Tests to check correct operation of Unity Test Framework
+ * @copyright Copyright (c) 2022
  */
 
 //-----------------------------------------------------------------
@@ -12,9 +12,7 @@
 //-----------------------------------------------------------------
 // Includes
 //-----------------------------------------------------------------
-
 #include "unity.h"
-#include "unity_fixture.h"
 
 //-----------------------------------------------------------------
 // Constant Definitions
@@ -40,12 +38,27 @@
 //-----------------------------------------------------------------
 //=================================================================
 
-TEST_GROUP_RUNNER(UnityTests)
+void setUp() { }
+
+void tearDown() { }
+
+void testAssertTrue(void)
 {
-    RUN_TEST_CASE(UnityTests, TestTrue);
-    RUN_TEST_CASE(UnityTests, TestFalse);
+    TEST_ASSERT_TRUE(1 == 1);
 }
 
+void testAssertFalse(void)
+{
+    TEST_ASSERT_FALSE(1 == 2);
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    RUN_TEST(testAssertTrue);
+    RUN_TEST(testAssertFalse);
+    return UNITY_END();
+}
 //-----------------------------------------------------------------
 // End Of File
 //-----------------------------------------------------------------
