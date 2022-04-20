f9c2b52719051464761f8d6ff9f1695f6be36bdf
Pete Alexander <peter.d.alexander23@gmail.com>
2022-04-20 23:23:55 +0100

    WIP: Add usb descriptors and config for test CDC
    Rewrite the usb descriptors and config from scratch to make a CDC
    interface and learn how everything goes together.
    
    This has not been tested on the real MCU!!
    This has not been documented correctly!!

--- hal/src/tusb_descriptors.cpp
+++ hal/src/tusb_descriptors.cpp
@@ -1,160 +1,169 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
+/**
+ * @file    tusb_descriptors.cpp
+ * @author  Peter Alexander (peter.d.alexander23@gmail.com)
+ * @brief   Definitions and callbacks for USB Descriptors
+ * @copyright Copyright (c) 2022
  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//-----------------------------------------------------------------
+// Preprocessor Switches
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------
 
 #include "tusb.h"
-#include "usb_descriptors.h"
 
-/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
- * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
- *
- * Auto ProductID layout's Bitmap:
- *   [MSB]         HID | MSC | CDC          [LSB]
- */
+//-----------------------------------------------------------------
+// Constant Definitions
+//-----------------------------------------------------------------
+
 #define _PID_MAP(itf, n) ((CFG_TUD_##itf) << (n))
 #define USB_PID (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4))
 
-#define USB_VID 0xCafe
-#define USB_BCD 0x0200
+#define USB_VID 0xBeef
+#define USB_BCD 0x200
 
-//--------------------------------------------------------------------+
-// Device Descriptors
-//--------------------------------------------------------------------+
 tusb_desc_device_t const desc_device = {
     .bLength = sizeof(tusb_desc_device_t),
     .bDescriptorType = TUSB_DESC_DEVICE,
     .bcdUSB = USB_BCD,
-    .bDeviceClass = 0x00,
-    .bDeviceSubClass = 0x00,
-    .bDeviceProtocol = 0x00,
+
+    .bDeviceClass = TUSB_CLASS_MISC,
+    .bDeviceSubClass = MISC_SUBCLASS_COMMON,
+    .bDeviceProtocol = MISC_PROTOCOL_IAD,
+
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
     .idVendor = USB_VID,
     .idProduct = USB_PID,
-    .bcdDevice = 0x0100,
+    .bcdDevice = 0x0010,
 
     .iManufacturer = 0x01,
     .iProduct = 0x02,
     .iSerialNumber = 0x03,
 
-    .bNumConfigurations = 0x01
+    .bNumConfigurations = 0x01,
 };
 
-// Invoked when received GET DEVICE DESCRIPTOR
-// Application return pointer to descriptor
-uint8_t const* tud_descriptor_device_cb(void)
-{
-    return (uint8_t const*)&desc_device;
-}
+// Define Endpoint numbers for the CDC interface
+#define EPNUM_CDC_NOTIF 0x81
+#define EPNUM_CDC_OUT 0x02
+#define EPNUM_CDC_IN 0x82
 
-//--------------------------------------------------------------------+
-// HID Report Descriptor
-//--------------------------------------------------------------------+
-
-uint8_t const desc_hid_report[] = {
-    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(REPORT_ID_KEYBOARD)),
-    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(REPORT_ID_MOUSE)),
-    TUD_HID_REPORT_DESC_CONSUMER(HID_REPORT_ID(REPORT_ID_CONSUMER_CONTROL)),
-    TUD_HID_REPORT_DESC_GAMEPAD(HID_REPORT_ID(REPORT_ID_GAMEPAD))
-};
-
-// Invoked when received GET HID REPORT DESCRIPTOR
-// Application return pointer to descriptor
-// Descriptor contents must exist long enough for transfer to complete
-uint8_t const* tud_hid_descriptor_report_cb(uint8_t instance)
-{
-    (void)instance;
-    return desc_hid_report;
-}
-
-//--------------------------------------------------------------------+
-// Configuration Descriptor
-//--------------------------------------------------------------------+
-
+// Assign a number to each of the interfaces present starting from 0.
+// Add a final value that automatically gives the number of interfaces present.
 enum {
-    ITF_NUM_HID,
-    ITF_NUM_TOTAL
+    ITF_NUM_CDC = 0,
+    ITF_NUM_CDC_DATA,
+    ITF_NUM_TOTAL,
 };
 
-#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_CDC_DESC_LEN)
 
-#define EPNUM_HID 0x81
+uint8_t const desc_fs_configuration[] {
+    // Configuration Descriptor
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
 
-uint8_t const desc_configuration[] = {
-    // Config number, interface count, string index, total length, attribute, power in mA
-    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
-
-    // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
-    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 5)
+    // CDC Interface Descriptor
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, EPNUM_CDC_NOTIF, 8, EPNUM_CDC_OUT, EPNUM_CDC_IN, 64),
 };
 
+// Definitions for High Speed Configuration
 #if TUD_OPT_HIGH_SPEED
-// Per USB specs: high speed capable device must report device_qualifier and other_speed_configuration
 
-// other speed configuration
-uint8_t desc_other_speed_config[CONFIG_TOTAL_LEN];
+uint8_t const desc_hs_configuration[] = {
+    // Configuration Descriptor, the same as the Full Speed version
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
 
-// device qualifier is mostly similar to device descriptor since we don't change configuration based on speed
+    // CDC Interface Descriptor, The endpoint size has been increased to 512 bytes
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, EPNUM_CDC_NOTIF, 8, EPNUM_CDC_OUT, EPNUM_CDC_IN, 512),
+};
+
 tusb_desc_device_qualifier_t const desc_device_qualifier = {
     .bLength = sizeof(tusb_desc_device_qualifier_t),
     .bDescriptorType = TUSB_DESC_DEVICE_QUALIFIER,
     .bcdUSB = USB_BCD,
 
-    .bDeviceClass = 0x00,
-    .bDeviceSubClass = 0x00,
-    .bDeviceProtocol = 0x00,
+    .bDeviceClass = TUSB_CLASS_MISC,
+    .bDeviceSubClass = MISC_SUBCLASS_COMMON,
+    .bDeviceProtocol = MISC_PROTOCOL_IAD,
 
     .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
     .bNumConfigurations = 0x01,
-    .bReserved = 0x00
+    .bReserved = 0x00,
 };
 
-// Invoked when received GET DEVICE QUALIFIER DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete.
-// device_qualifier descriptor describes information about a high-speed capable device that would
-// change if the device were operating at the other speed. If not highspeed capable stall this request.
+// array of pointer to string descriptors
+char const* string_desc_arr[] = {
+    (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+    "TinyUSB", // 1: Manufacturer
+    "TinyUSB Device", // 2: Product
+    "123456789012", // 3: Serials, should use chip ID
+    "TinyUSB CDC", // 4: CDC Interface
+};
+
+//-----------------------------------------------------------------
+// Type Definitions
+//-----------------------------------------------------------------
+
+// None
+
+//-----------------------------------------------------------------
+// Private Function Prototypes
+//-----------------------------------------------------------------
+
+// None
+
+//=================================================================
+//-----------------------------------------------------------------
+// Private Functions
+//-----------------------------------------------------------------
+//=================================================================
+
+//=================================================================
+//-----------------------------------------------------------------
+// Public Functions
+//-----------------------------------------------------------------
+//=================================================================
+
+uint8_t const* tud_descriptor_device_cb(void)
+{
+    return (uint8_t const*)&desc_device;
+}
+
+// Call back to hand the device qualifier to TinyUSB
 uint8_t const* tud_descriptor_device_qualifier_cb(void)
 {
     return (uint8_t const*)&desc_device_qualifier;
 }
 
-// Invoked when received GET OTHER SEED CONFIGURATION DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-// Configuration descriptor in the other speed e.g if high speed then this is for full speed and vice versa
+// Define a space for the "other" speed configuration
+uint8_t desc_other_speed_config[CONFIG_TOTAL_LEN];
+
+// Call back to hand the descriptor for the "other" speed to TinyUSB
+// This is the descriptor that we aren't using.
 uint8_t const* tud_descriptor_other_speed_configuration_cb(uint8_t index)
 {
     (void)index; // for multiple configurations
 
-    // other speed config is basically configuration with type = OHER_SPEED_CONFIG
-    memcpy(desc_other_speed_config, desc_configuration, CONFIG_TOTAL_LEN);
+    // if link speed is high return fullspeed config, and vice versa
+    // Note: the descriptor type is OTHER_SPEED_CONFIG instead of CONFIG
+    memcpy(desc_other_speed_config,
+        (tud_speed_get() == TUSB_SPEED_HIGH) ? desc_fs_configuration : desc_hs_configuration,
+        CONFIG_TOTAL_LEN);
+
     desc_other_speed_config[1] = TUSB_DESC_OTHER_SPEED_CONFIG;
 
-    // this example use the same configuration for both high and full speed mode
     return desc_other_speed_config;
 }
 
-#endif // highspeed
+#endif // TUD_OPT_HIGH_SPEED
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
@@ -163,21 +172,13 @@ uint8_t const* tud_descriptor_configuration_cb(uint8_t index)
 {
     (void)index; // for multiple configurations
 
-    // This example use the same configuration for both high and full speed mode
-    return desc_configuration;
+#if TUD_OPT_HIGH_SPEED
+    // Although we are highspeed, host may be fullspeed.
+    return (tud_speed_get() == TUSB_SPEED_HIGH) ? desc_hs_configuration : desc_fs_configuration;
+#else
+    return desc_fs_configuration;
+#endif
 }
-
-//--------------------------------------------------------------------+
-// String Descriptors
-//--------------------------------------------------------------------+
-
-// array of pointer to string descriptors
-char const* string_desc_arr[] = {
-    (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-    "TinyUSB", // 1: Manufacturer
-    "TinyUSB Device", // 2: Product
-    "123456", // 3: Serials, should use chip ID
-};
 
 static uint16_t _desc_str[32];
 
@@ -202,7 +203,7 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
         const char* str = string_desc_arr[index];
 
         // Cap at max char
-        chr_count = strlen(str);
+        chr_count = (uint8_t)strlen(str);
         if (chr_count > 31)
             chr_count = 31;
 
@@ -217,3 +218,12 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 
     return _desc_str;
 }
+//-----------------------------------------------------------------
+
+#ifdef __cplusplus
+}
+#endif
+
+//-----------------------------------------------------------------
+// End Of File
+//-----------------------------------------------------------------
